7996e8bca882ae5085d2f4311d243f4c5c484b29
John Gamboa <johnhadriansgamboa@gmail.com>
2016-05-15 15:21:24 -0700

    got parallax to work -- finally

---
+++ js/parallax.js
@@ -0,0 +1,22 @@
+$(function() {
+
+  $(document).ready(function() {
+
+    $('.mdl-layout, .mdl-layout__content').stellar({
+      horizontalScrolling: false,
+      parallaxBackgrounds: true,
+      positionProperty: 'transform',
+      responsive: true
+    });
+
+    ($(window).width() > 1024) ? $('#landing').attr('data-stellar-background-ratio', '1.6')
+                               : $('#landing').removeAttr('data-stellar-background-ratio');
+  });
+
+  $(window).resize(function() {
+    ($(window).width() > 1024) ? $('#landing').attr('data-stellar-background-ratio', '1.6')
+                               : $('#landing').removeAttr('data-stellar-background-ratio');
+
+    $.stellar('refresh');
+  });
+});
