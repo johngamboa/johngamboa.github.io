500d72198a744fd3c7654c4a6811d2434fe30a3c
John Gamboa <johnhadriansgamboa@gmail.com>
2016-05-15 16:20:22 -0700

    fix for mobile mountains disappearing after scrolling down

--- js/parallax.js
+++ js/parallax.js
@@ -9,12 +9,12 @@ $(function() {
       responsive: true
     });
 
-    ($(window).width() > 1024) ? $('#landing').attr('data-stellar-background-ratio', '1.6')
+    ($(window).width() > 1024) ? $('#landing').attr('data-stellar-background-ratio', '1.5')
                                : $('#landing').removeAttr('data-stellar-background-ratio');
   });
 
   $(window).resize(function() {
-    ($(window).width() > 1024) ? $('#landing').attr('data-stellar-background-ratio', '1.6')
+    ($(window).width() > 1024) ? $('#landing').attr('data-stellar-background-ratio', '1.5')
                                : $('#landing').removeAttr('data-stellar-background-ratio');
 
     $.stellar('refresh');
