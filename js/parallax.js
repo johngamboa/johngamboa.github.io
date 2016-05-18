7d9a2fb62f9c9a7f095b809a23dbf3ad2e06d383
John Gamboa <johnhadriansgamboa@gmail.com>
2016-05-17 19:42:59 -0700

    added mini me

--- js/parallax.js
+++ js/parallax.js
@@ -16,7 +16,7 @@ $(function() {
 
   $(window).resize(function() {
     if ($(window).width() > 1024) {
-      $('#landing').attr('data-stellar-background-ratio', '1.25');
+      $('#landing').attr('data-stellar-background-ratio', '1.5');
       $('#landing').css('background-position', 'top, top');
     }
     else {
