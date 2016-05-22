504d5faefe1ee4872339bf0e7c4454b01c7a9101
John Gamboa <johnhadriansgamboa@gmail.com>
2016-05-21 18:13:57 -0700

    attempt at fix for parallax scrolling (once again...)

--- js/parallax.js
+++ js/parallax.js
@@ -1,28 +1,25 @@
 $(function() {
 
-  var desktopWidth = 1024,
-      landing       = $('landing');
+  var landing = $('#landing');
 
   $('.mdl-layout, .mdl-layout__content').stellar({
     horizontalScrolling : false,
-    parallaxBackgrounds : true,
     responsive          : true
   });
 
   $(document).ready(function() {
-    ($(window).width() > desktopWidth) ? landing.attr('data-stellar-background-ratio', '0.35')
-                                       : landing.removeAttr('data-stellar-background-ratio');
+    ($(window).width() > 1024) ? landing.attr('data-stellar-background-ratio', '0.5')
+                               : landing.removeAttr('data-stellar-background-ratio');
 
     $.stellar('refresh');
   });
 
   $(window).resize(function() {
-    if ($(window).width() > desktopWidth) {
-      landing.attr('data-stellar-background-ratio', '0.35');
-      landing.css('background-position', 'top, top');
+    console.log($(window).width());
+    if ($(window).width() > 1024) {
+      landing.attr('data-stellar-background-ratio', '0.5');
     }
     else {
-      landing.css('background-position', 'bottom, bottom');
       landing.removeAttr('data-stellar-background-ratio');
     }
 
